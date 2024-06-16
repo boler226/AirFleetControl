afb84205300556f0ab73310cdca07bb355bb959f
boler226 <illamatviuk@gmail.com>
2024-06-17 00:18:28 +0300

    Add objects and Constructors to Flight.h

---
+++ AirFleetControl/Flight.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <ctime>
+#include "Destination.h";
+#include "Plane.h";
+
+class Flight
+{
+private:
+	int flightNumber;	// Номер рейсу
+	Destination whence;   // Звідки
+	Destination whither;   // Куди
+	std::vector<Destination> intermeditateStops;	// Пункти проміжної посадки
+	std::time_t departureTime;	// Час відправлення
+	std::time_t  flightDays;	// Дні польоту
+	int availableSeats;	// Кількість вільних місць
+	Plane plane;	// Літак
+public:
+	Flight();	// Конструктор за замовчуванням
+	Flight(   // Конструктор з параметрами
+		int num,
+		Destination& whence,
+		Destination& whither,
+		const std::vector<Destination>& stops,
+		const std::time_t& time, 
+		const std::time_t& days, 
+		int seats, 
+		Plane& plane
+	);
+	Flight(const Flight& other);	// Конструктор копіювання
+	Flight(Flight&& other) noexcept;	// Конструктор переміщення
+	~Flight();	// Деструктор
+
+};
+
